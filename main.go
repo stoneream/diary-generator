49eab8c7906bdcfd2c7abf8a6164877043051eba
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-08-08 02:33:50 +0900

    impl Initコマンド

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"flag"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/google/subcommands"
 )
 
 type initCmd struct {
-	config string
+	baseDirectory string
+	templateFile  string
 }
 
 func (*initCmd) Name() string     { return "init" }
@@ -20,35 +23,57 @@ func (*initCmd) Usage() string {
 `
 }
 func (p *initCmd) SetFlags(f *flag.FlagSet) {
-	f.StringVar(&p.config, "config", "", "config file")
+	f.StringVar(&p.baseDirectory, "base-directory", "", "base directory")
+	f.StringVar(&p.templateFile, "template-file", "", "template file path")
 }
-func (*initCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	// TODO implement
-	return subcommands.ExitSuccess
-}
+func (p *initCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	if p.baseDirectory == "" || p.templateFile == "" {
+		return subcommands.ExitUsageError
+	}
 
-type archiveCmd struct {
-	config string
-}
+	now := time.Now()
+	targetDirName := now.Format("2006-01-02")
+	templateFileName := filepath.Base(p.templateFile)
+	targetFilePath := filepath.Join(p.baseDirectory, targetDirName, templateFileName)
 
-func (*archiveCmd) Name() string     { return "archive" }
-func (*archiveCmd) Synopsis() string { return "Archive a diary" }
-func (*archiveCmd) Usage() string {
-	return `archive:
-	Archive a diary.
-`
-}
-func (p *archiveCmd) SetFlags(f *flag.FlagSet) {
-	f.StringVar(&p.config, "config", "", "config file")
-}
-func (*archiveCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	// TODO implement
+	// テンプレートファイルの存在チェック
+	_, err := os.Stat(templateFileName)
+	if err != nil {
+		return subcommands.ExitFailure
+	}
+
+	// 出力先ディレクトリの存在チェック
+	_, err = os.Stat(filepath.Dir(targetFilePath))
+	if err != nil {
+		// ディレクトリが存在しない場合は作成
+		err = os.MkdirAll(filepath.Dir(targetFilePath), 0755)
+		if err != nil {
+			return subcommands.ExitFailure
+		}
+	}
+
+	// テンプレートファイルをコピー
+	input, err := os.ReadFile(p.templateFile)
+	if err != nil {
+		return subcommands.ExitFailure
+	}
+
+	output, err := os.Create(targetFilePath)
+	if err != nil {
+		return subcommands.ExitFailure
+	}
+	defer output.Close()
+
+	_, err = output.Write(input)
+	if err != nil {
+		return subcommands.ExitFailure
+	}
+
 	return subcommands.ExitSuccess
 }
 
 func main() {
 	subcommands.Register(&initCmd{}, "")
-	subcommands.Register(&archiveCmd{}, "")
 
 	flag.Parse()
 	ctx := context.Background()
