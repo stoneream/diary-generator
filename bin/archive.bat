fc9890f9505bd250e0a3a31ff011717b10d8c221
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2024-08-11 23:25:03 +0900

    移植が済んだのでscalaのソースを削除

--- bin/archive.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-set TARGET=%1
-set STARTSWITH=%2
-
-cd /d %~dp0
-
-java -jar .\diary-generator.jar archive --base-directory-path ..\%TARGET% --starts-with %STARTSWITH%
--- bin/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TARGET=$1
-
-cd $(dirname $0)
-
-java -jar ./diary-generator.jar init --base-directory-path ../$TARGET --template-path ./template/$TARGET.md
